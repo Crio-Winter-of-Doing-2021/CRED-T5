812fc15177e8c86d208a971c670630d5e467922e
Chinmay Chougaonkar <chinmay0402@gmail.com>
2021-04-14 14:55:38 +0530

    Resolve infinite requests issue

--- frontend/src/components/Reward.js
+++ frontend/src/components/Reward.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { withStyles, TableCell, TableRow, Button } from '@material-ui/core';
 
-export default function Reward({ reward, coin_bal }) {
+export default function Reward({ reward, coin_bal, track, setTrack }) {
     const StyledTableCell = withStyles((theme) => ({
         head: {
             backgroundColor: theme.palette.common.black,
@@ -20,7 +20,6 @@ export default function Reward({ reward, coin_bal }) {
     }))(TableRow);
     const { cost, reward_id, body } = reward;
     const buyReward = async () => {
-        console.log("123");
         const token = localStorage.token;
         try {
             const response = await fetch(`http://localhost:8080/rewards/buy/${reward_id}`, {
@@ -31,6 +30,7 @@ export default function Reward({ reward, coin_bal }) {
             });
             const parseRes = await response.json();
             console.log(parseRes);
+            setTrack(track + 1);
         } catch (err) {
             console.log(err);
         }
